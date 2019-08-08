01c1af2563d77fbde7313230032d06708115444e
little_cup <heruoxin@users.noreply.github.com>
2019-08-08 21:14:13 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 setTimeout(() => {
     document.body.innerHTML = document.body.innerHTML.replace(/iceboxdoc.catchingnow.com/g, window.location.host);
     if (/\.cn$/.test(window.location.hostname)) {
-        document.body.innerHTML = document.body.innerHTML.replace(/<small>Hosted on GitHub Pages.*<\/small>/g, `<a href="http://beian.miit.gov.cn">蜀ICP备19024627号-1</a>`)
+        document.body.innerHTML = document.body.innerHTML.replace(/<small>Hosted on GitHub Pages.*<\/small>/g, `<a href="http://beian.miit.gov.cn">蜀ICP备19024627号</a>`)
     }
 }, 800);
